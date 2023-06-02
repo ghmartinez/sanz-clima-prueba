113c503c08ee2675e88d12b7b982d3149932eff4
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-06-02 19:12:12 +0200

    After creating the new rails app, I created the routing of the application. As there's not going to be a database for this app (because it is not needed and it is not demanded in the test), there is no need for creating models of any kind. The routing then only consists in a 'pages' controller that includes the methods and views for home, calc and history.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'pages#home'
+  get '/calc', to: 'pages#calc'
+  get '/history', to: 'pages#history'
 end
