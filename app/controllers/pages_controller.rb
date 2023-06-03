462ea90df5a5a1e40d5f3e6cf252c93189a015dc
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-06-03 22:43:26 +0200

    Small fix for calculation method.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,12 @@ class PagesController < ApplicationController
   end
 
   def history
+    if params.has_key?(:numbers)
     number1 = params[:numbers][:number1]
     number2 = params[:numbers][:number2]
     @result = number1.to_i + number2.to_i
+    else
+      @result = "yet to be done"
+    end
   end
 end
